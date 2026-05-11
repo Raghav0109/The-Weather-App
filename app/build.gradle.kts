eec7ca20255419507279381c9e8db8030ea9a60a
Raghav0109 <sawantra004@gmail.com>
2026-05-12 00:35:06 +0530

    WeatherApi part completed

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -71,4 +71,6 @@ dependencies {
     implementation(libs.converter.gson)
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.1")
     implementation("androidx.navigation:navigation-compose:2.7.1")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
 }
